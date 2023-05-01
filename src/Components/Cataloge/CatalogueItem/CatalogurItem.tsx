c7a708806777bd414efc5c533ab046c2c628fa62
Sinamon-maker <nikaterina@gmail.com>
2023-05-01 07:14:04 -0700

    catalogueItem modified

--- src/Components/Cataloge/CatalogueItem/CatalogurItem.tsx
+++ src/Components/Cataloge/CatalogueItem/CatalogurItem.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const CatalogueItem = ({ docum, logoName }: Props) => {
 	if (logoName && docum.userId === logoName.uid) {
 		return (
-			<Link className="w-full grow hover:scale-105 transition-all" to={`/tasks/${docum.id}`}>
+			<Link className="w-full grow hover:scale-105 hover:shadow-lg hover:transition-all transition-all" to={`/tasks/${docum.id}`}>
 				<div className="w-full  p-4 rounded-lg border flex gap-4 items-center text-skin-base">
 					<div className="grow">{docum.title}</div>
 					<div>{docum.tasks.length}</div>
